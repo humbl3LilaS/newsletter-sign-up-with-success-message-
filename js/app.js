bc3cb676bd04cd235b92c0147158293355ae75d2
humbl3LilaS <sabishinekobebe@gmail.com>
2023-12-23 20:42:49 +0630

    success email response repair

--- js/app.js
+++ js/app.js
@@ -3,6 +3,7 @@ const btn = document.getElementById("btn");
 const newsLetter = document.getElementById("newsLetterContainer");
 const success = document.getElementById("success");
 const isValid = input.validity.valid;
+const mail = document.getElementById("mail");
 const regEx = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
 
 console.log("test", regEx.test("super"));
@@ -11,7 +12,8 @@ btn.addEventListener("click", () => {
   if (isValid) {
 	if (regEx.test(input.value)) {
 	  newsLetter.classList.toggle("hidden");
-	  success.classList.toggle("hidden")
+	  success.classList.toggle("hidden");
+	  mail.innerText = input.value;
 	}
   }
 });
